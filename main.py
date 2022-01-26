7a822b3adbb964f8e0b20643514fc777aa14be37
HarganFire <82879846+HarganFire@users.noreply.github.com>
2022-01-26 13:17:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from nextcord.ext import commands
 import os
 import asyncio
 import random
-from Tools.utils import getLanguage, getConfig
+
 import json
 
 with open("config.json", "r") as config:
-        config = json.load(config)
-
-
+    config = json.load(config)
 
 client = commands.Bot(command_prefix='!')
 client.remove_command("help")
@@ -36,12 +34,23 @@ async def ch_pr():
             count = count + 1
 
     while not client.is_closed():
-        await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.playing, name=f"regarder son écran"))
+        await client.change_presence(
+            activity=nextcord.Activity(type=nextcord.ActivityType.playing, name=f"regarder son écran"))
 
         await asyncio.sleep(30)
 
 
 client.loop.create_task(ch_pr())
+
+botversion = "beta"
+footer = f"Wavys {botversion}"
+icon = "https://cdn.discordapp.com/attachments/576032647806058503/935612382414245918/Frame_1889.png"
+owners = ['597680465025040425', '638730315950587914']
+c_em_b = 0xEAB38D  # couleur d'embed basique
+c_em_e = 0xb51f18  # couleur d'embed error
+c_em_s = 0x31ba18  # couleur d'embed success
+
+
 
 @client.command()
 async def load(ctx, extension):
@@ -57,8 +66,12 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-for filename in os.listdir('cogs/Membre'):
+for filename in os.listdir('./cogs/utils'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.Membre.{filename[:-3]}')
+        client.load_extension(f'cogs.utils.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/mod'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.mod.{filename[:-3]}')
 
 client.run(config["token"])
